c025770803305c996d03082e9a7f29b095d8a7e7
dalilahonic <dalilahonic1@gmail.com>
2023-12-29 21:11:45 +0100

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css';
 import HeaderMain from './components/Main Page/HeaderMain';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Writing from './pages/Writing';
+import Speaking from './pages/Speaking';
+import Post from './pages/Post';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HeaderMain />,
+  },
+  { path: '/writing', element: <Writing /> },
+  { path: '/speaking', element: <Speaking /> },
+  { path: '/spking/:id', element: <Post /> },
+]);
 
 function App() {
-  return (
-    <>
-      <HeaderMain />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/components/Main Page/HeaderMain.jsx
+++ frontend/src/components/Main Page/HeaderMain.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react';
 import classes from './HeaderMain.module.css';
+import { Link } from 'react-router-dom';
 
 export default function HeaderMain() {
+  const [darkTheme, setDarkTheme] = useState(true);
+
+  function changeTheme() {}
+
   return (
     <div className={classes.main}>
       <nav>
-        <div className={classes.leftNavigation}></div>
+        <div
+          className={`${classes.leftNavigation} ${
+            darkTheme ? classes.dark : classes.light
+          }`}
+        ></div>
         <div className={classes.rightNavigation}>
           <ul>
-            <li>writing</li>
-            <li>speaking</li>
+            <Link to='/writing'>
+              <li>writing</li>
+            </Link>
+            <Link to='/speaking'>
+              <li>speaking</li>
+            </Link>
             <li>workshop</li>
             <li>playing</li>
-            <button></button>
+            <div
+              onClick={changeTheme}
+              className={classes.button}
+            ></div>
           </ul>
         </div>
       </nav>
